52f04771d05e5489a506b2ef82912ccf540123e6
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-06-04 20:48:12 -0300

    ngoptimizedimage angular study

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent],
+  providers: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
