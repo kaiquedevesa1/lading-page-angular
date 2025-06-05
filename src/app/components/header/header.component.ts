31e5bb169afdbd4264bcafee941d30ea76d9ac3d
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-06-05 20:26:19 -0300

    style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [NgOptimizedImage],
+  standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  standalone: true,
+  imports: [HeaderComponent, NgOptimizedImage],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
