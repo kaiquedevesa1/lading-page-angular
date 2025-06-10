466787736c3f050537f46393b8f009d313550166
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-06-09 23:11:12 -0300

    styled home component

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter-form',
+  imports: [],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+
+}
