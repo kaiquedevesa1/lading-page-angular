7b2f9a5b751910c21a3546e6e2e8acb741877331
kaiquedevesa1 <kaikedaeves@hotmail.com>
2025-06-14 20:30:00 -0300

    res api

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface NewsletterResponse {
+  message: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class NewsletterService {
-  private endpointUrl =
-    'https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/';
+  private endpointUrl = 'https://httpbin.org/post';
 
   constructor(private http: HttpClient) {}
 
